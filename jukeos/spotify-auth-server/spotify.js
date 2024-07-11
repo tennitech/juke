f8089c97c96ce8e0eee68cf60f0f5b2d21a8a81d
Daniel Park <parkd7@rpi.edu>
2024-07-11 17:37:22 -0500

    Add generatedRandomString in spotify.js

---
+++ jukeos/spotify-auth-server/spotify.js
@@ -0,0 +1,13 @@
+function generateRandomString(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        let index = Math.floor(Math.random() * possible.length);
+        text += possible.charAt(index);
+    }
+
+    return text;
+}
+
+
