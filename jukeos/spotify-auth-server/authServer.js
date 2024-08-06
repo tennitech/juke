77f5e9dc36557eb4c1188f24c02b1fa3a185f039
Daniel Park <parkd7@rpi.edu>
2024-08-06 18:09:17 -0500

    Add getToken in authServer.js

--- jukeos/spotify-auth-server/authServer.js
+++ jukeos/spotify-auth-server/authServer.js
@@ -1,6 +1,22 @@
-const getAuthToken = async (code) => {
+const getToken = async code => {
+    let codeVerifier = localStorage.getItem('code_verifier');
+    const payload = {
+        method: 'POST', 
+        headers: {'Content Type': 'application/x-www-form-urlencoded'}, 
+        body: new URLSearchParams({
+            client_id: clientId, 
+            grant_type: 'authorization_code',
+            code, 
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+        }),
+    }
 
+    const body = await fetch(url, payload);
+    const response = await body.json();
+
+    localStorage.setItem('access_token', response.access_token);
 }
 
 
-export default getAuthToken;
+export default getToken;
