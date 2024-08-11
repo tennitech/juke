34776fa0b0cd96ce8baec4dfcf9754345884b520
Daniel Park <parkd7@rpi.edu>
2024-08-10 22:21:35 -0500

    Move getToken to spotify.js

--- jukeos/spotify-auth-server/spotify.js
+++ jukeos/spotify-auth-server/spotify.js
@@ -62,4 +62,27 @@ async function requestAuthorization() {
 }
 
 
-export default requestAuthorization;
+const getToken = async code => {
+    requestAuthorization();
+
+    let codeVerifier = localStorage.getItem('code_verifier');
+    const payload = {
+        method: 'POST', 
+        headers: {'Content Type': 'application/x-www-form-urlencoded'}, 
+        body: new URLSearchParams({
+            client_id: clientId, 
+            grant_type: 'authorization_code',
+            code, 
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+        }),
+    }
+
+    const body = await fetch(url, payload);
+    const response = await body.json();
+
+    localStorage.setItem('access_token', response.access_token);
+}
+
+
+export default getToken;
