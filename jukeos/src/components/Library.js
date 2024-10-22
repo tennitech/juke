704e5bfa3816b4ef9e525f2707d4619dac08acef
Kaden <86852414+tennitech@users.noreply.github.com>
2024-10-22 18:07:50 -0400

    Implemented template/framework for future section titles
    On Library.js, I implemented the section title for PLAYLISTS. This will serve as the template for the other titles.
    
    After lots of trial and error, I've been able to use an SVG and curve the text, to appear similar to our mockups. I also integrated a library called react-curved-text, however, I decided to forego that in favor of using SVGs.

--- jukeos/src/components/Library.js
+++ jukeos/src/components/Library.js
@@ -31,10 +31,27 @@ const Library = () => {
       backgroundImage: `url(${backgroundPng})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
       minHeight: '100vh',
-      padding: '20px'
+      padding: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
     }}>
-      <h1 className="glow">PLAYLISTS</h1>
+      <div style={{ marginTop: '100px', position: 'relative', width: '800px', height: '800px' }}>
+        <svg width="800" height="800" style={{ position: 'absolute', top: 0, left: 0 }}>
+          <defs>
+            <path id="circlePath" d="M 400,400 m 0,300 a 300,300 0 1,1 0,-600 a 300,300 0 1,1 0,600" />
+          </defs>
+          <text fill="#ECE0C4" fontFamily="Loubag" fontSize="50">
+            <textPath href="#circlePath" startOffset="50%" textAnchor="middle">
+              <tspan dy="10" dx="0" transform="rotate(90 400 400)">PLAYLISTS</tspan>
+            </textPath>
+          </text>
+        </svg>
+      </div>
       <div>
         {savedTracks.length ? (
           <ul>
