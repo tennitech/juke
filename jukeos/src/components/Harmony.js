a974ba4377eeb9ed05eebe57d2050edefd987908
Daniel Park <parkdaniel1002@gmail.com>
2025-02-28 17:37:30 -0500

    Add simple test for Google Gemini API on `Harmony.js`

--- jukeos/src/components/Harmony.js
+++ jukeos/src/components/Harmony.js
@@ -1,3 +1,14 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+
+const genAI = new GoogleGenerativeAI("DON'T YOU THINK.");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Can you tell me the difference between Google's Cirq and IBM's Qiskit?";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
+
 const Harmony = () => {
   return;
 };
