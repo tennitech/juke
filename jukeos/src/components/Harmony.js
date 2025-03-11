5e4549ed150656f6ddc9195202e4d4cdd7b75e06
Daniel Park <parkdaniel1002@gmail.com>
2025-03-11 17:30:07 -0400

    Add styles in prompt response

--- jukeos/src/components/Harmony.js
+++ jukeos/src/components/Harmony.js
@@ -1,16 +1,19 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 
-const genAI = new GoogleGenerativeAI("DON'T YOU THINK.");
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const prompt = "Can you tell me the difference between Google's Cirq and IBM's Qiskit?";
-
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
-
 const Harmony = () => {
-  return;
+  // const genAI = new GoogleGenerativeAI("DON'T YOU THINK.");
+  // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  // const prompt = "Can you tell me the difference between Google's Cirq and IBM's Qiskit?";
+  // const result = await model.generateContent(prompt);
+  // console.log(result.response.text());
+
+  return (<p style={({
+    color: 'white'
+  })}>
+    Hello, world!
+  </p>);
 };
+
 
 export default Harmony;
